5d34cc7600856281f3c2c23ab710f9659677baa5
daveKidd <david.kidd@lambdaschool.com>
2021-02-16 11:59:29 -0700

    ketchup

--- data/migrations/20210216110019_fruits_table.js
+++ data/migrations/20210216110019_fruits_table.js
@@ -3,6 +3,8 @@ exports.up = function(knex) {
     return knex.schema.createTable("fruits", table =>{
         table.increments()
         table.text("name",128).unique().notNullable()
+        table.decimal("avgWeightOz")
+        table.boolean("delicious")
     })
 };
 
